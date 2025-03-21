946468e1a7910841dadb55254e7b3a02a46dd406
VietHungUET <124850661+VietHungUET@users.noreply.github.com>
2025-03-21 21:58:43 +0700

    all use

--- src/test/java/ShippingCostCalculatorTest.java
+++ src/test/java/ShippingCostCalculatorTest.java
@@ -19,4 +19,24 @@ public class ShippingCostCalculatorTest {
     void testCalculateShippingCost(int weight, int distance, double expected) {
         assertEquals(expected, ShippingCostCalculator.calculateShippingCost(weight, distance), 1e-9);
     }
+
+    // Kiểm thử với độ phủ all use
+    @ParameterizedTest
+    @CsvSource({
+            "-7, 7 , -1",
+            "45, 20, 65000",
+            "4, 20, 30000",
+            "25, 20, 50000",
+            "10, -5, -1",
+            "45, 35, 104500 ",
+            "35, 20, 50000",
+            "45, 25, 75000",
+            "35, 3, 24500",
+            "25, 3, 24500",
+            "35, 25, 57500 ",
+            "35, 35, 79750",
+    })
+    void all_use_testCalculateShippingCost(int weight, int distance, double expected) {
+        assertEquals(expected, ShippingCostCalculator.calculateShippingCost(weight, distance), 1e-9);
+    }
 }

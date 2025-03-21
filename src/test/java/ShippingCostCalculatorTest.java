bd19088e717977f14edbd0db03683b0b0b1a30bb
VietHungUET <124850661+VietHungUET@users.noreply.github.com>
2025-03-21 21:24:05 +0700

    Optimize Testcase by using juint 5 instead of using junit 4

--- src/test/java/ShippingCostCalculatorTest.java
+++ src/test/java/ShippingCostCalculatorTest.java
@@ -1,42 +1,22 @@
 import org.example.ShippingCostCalculator;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShippingCostCalculatorTest {
 
-
-    @Test
-    public void testCalculateShippingCost1() {
-        assertEquals(-1, ShippingCostCalculator.calculateShippingCost(-1, 0), 1e-9);
-    }
-
-    @Test
-    public void testCalculateShippingCost2() {
-        assertEquals(45100, ShippingCostCalculator.calculateShippingCost(4, 31), 1e-9);
-    }
-
-    @Test
-    public void testCalculateShippingCost3() {
-        assertEquals(20000, ShippingCostCalculator.calculateShippingCost(4, 10), 1e-9);
-    }
-
-    @Test
-    public void testCalculateShippingCost4() {
-        assertEquals(73150, ShippingCostCalculator.calculateShippingCost(10, 31), 1e-9);
-    }
-
-    @Test
-    public void testCalculateShippingCost5() {
-        assertEquals(27500, ShippingCostCalculator.calculateShippingCost(10, 5), 1e-9);
-    }
-
-    @Test
-    public void testCalculateShippingCost6() {
-        assertEquals(95700, ShippingCostCalculator.calculateShippingCost(41, 31), 1e-9);
-    }
-
-    @Test
-    public void testCalculateShippingCost7() {
-        assertEquals(35000, ShippingCostCalculator.calculateShippingCost(41, 5), 1e-9);
+    @ParameterizedTest
+    @CsvSource({
+            "-1, 0, -1",
+            "4, 31, 45100",
+            "4, 10, 20000",
+            "10, 31, 73150",
+            "10, 5, 27500",
+            "41, 31, 95700",
+            "41, 5, 35000"
+    })
+    void testCalculateShippingCost(int weight, int distance, double expected) {
+        assertEquals(expected, ShippingCostCalculator.calculateShippingCost(weight, distance), 1e-9);
     }
 }
